83ac4c2f46fb36c131eb8f3d1817678c43cb3afb
Gaurav-021 <63582549+Gaurav-021@users.noreply.github.com>
2023-09-28 20:07:16 -0500

    working mecanum drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/work.java
@@ -0,0 +1,164 @@
+
+
+
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "pls work" ,group = "Linear Opmode")
+
+//@Disabled
+public class work extends LinearOpMode {
+    //Dashboard demo variables
+    public static double ORBITAL_FREQUENCY = 0.05;
+    public static double SPIN_FREQUENCY = 0.25;
+    public static double ORBITAL_RADIUS = 50;
+    public static double SIDE_LENGTH = 10;
+
+    //Motor demo variables
+    private DcMotorEx frontLeftDrive = null;
+    private DcMotorEx frontRightDrive = null;
+    private DcMotorEx backLeftDrive = null;
+    private DcMotorEx backRightDrive = null;
+
+    public void initializeMotors()
+    {
+        frontLeftDrive = hardwareMap.get(DcMotorEx.class, "frontleft");
+        frontRightDrive = hardwareMap.get(DcMotorEx.class, "frontright");
+        backLeftDrive = hardwareMap.get(DcMotorEx.class, "backleft");
+        backRightDrive = hardwareMap.get(DcMotorEx.class, "backright");
+
+        frontRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+
+    private static void rotatePoints(double[] xPoints, double[] yPoints, double angle) {
+        for (int i = 0; i < xPoints.length; i++) {
+            double x = xPoints[i];
+            double y = yPoints[i];
+            xPoints[i] = x * Math.cos(angle) - y * Math.sin(angle);
+            yPoints[i] = x * Math.sin(angle) + y * Math.cos(angle);
+        }
+    }
+
+
+    public void dashboardDemo(){
+        double time = getRuntime();
+
+        double bx = ORBITAL_RADIUS * Math.cos(2 * Math.PI * ORBITAL_FREQUENCY * time);
+        double by = ORBITAL_RADIUS * Math.sin(2 * Math.PI * ORBITAL_FREQUENCY * time);
+        double l = SIDE_LENGTH / 2;
+
+        double[] bxPoints = { l, -l, -l, l };
+        double[] byPoints = { l, l, -l, -l };
+        rotatePoints(bxPoints, byPoints, 2 * Math.PI * SPIN_FREQUENCY * time);
+        for (int i = 0; i < 4; i++) {
+            bxPoints[i] += bx;
+            byPoints[i] += by;
+        }
+
+
+        sleep(20);
+    }
+
+
+    private void joystickTankDrive()
+    {
+        float Lj_y;
+        float Rj_y;
+
+        Lj_y = gamepad1.left_stick_y;
+        Rj_y = gamepad1.right_stick_y;
+
+        frontLeftDrive.setPower(Lj_y);
+        frontRightDrive.setPower(Rj_y);
+        backLeftDrive.setPower(Lj_y);
+        backRightDrive.setPower(Rj_y);
+    }
+    private void joystickMecanumDrive() {
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = -(y + x + rx) / denominator; //A-RC has negative before parentheses and B-RC has no negative
+            double backLeftPower = -(y - x + rx) / denominator;
+            double frontRightPower = -(y - x - rx) / denominator;
+            double backRightPower = -(y + x - rx) / denominator;
+
+            frontLeftDrive.setPower(frontLeftPower);
+            backLeftDrive.setPower(backLeftPower);
+            frontRightDrive.setPower(frontRightPower);
+            backRightDrive.setPower(backRightPower);
+        }
+    }
+
+    private void joystickMecanumDriveFieldCentric() {
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers m    aintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = -(y + x + rx) / denominator;
+            double backLeftPower = -(y - x + rx) / denominator;
+            double frontRightPower = -(y - x - rx) / denominator;
+            double backRightPower = -(y + x - rx) / denominator;
+
+            frontLeftDrive.setPower(frontLeftPower);
+            backLeftDrive.setPower(backLeftPower);
+            frontRightDrive.setPower(frontRightPower);
+            backRightDrive.setPower(backRightPower);
+        }
+    }
+
+
+
+
+    public void runOpMode()  {
+        initializeMotors();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addData("FL (0)", frontLeftDrive.getCurrentPosition());
+            telemetry.addData("FR (1)", frontRightDrive.getCurrentPosition());
+            telemetry.addData("BL (2)", backLeftDrive.getCurrentPosition());
+            telemetry.addData("BR (3)", backRightDrive.getCurrentPosition());
+            updateTelemetry(telemetry);
+
+            //joystickTankDrive();
+            joystickMecanumDrive();
+//            dashboardDemo();
+        }
+    }
+}
